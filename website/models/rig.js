5406f8b3edfc5eb1389e2089da29e10827d889ad
Max Fuller <max.fuller01@btinternet.com>
2018-01-17 20:51:10 +0000

    Added approvedUssers to rig model

--- website/models/rig.js
+++ website/models/rig.js
@@ -1,7 +1,13 @@
 var mongoose = require("mongoose");
 
+//var User = require("../models/user");
+
 var rigSchema = new mongoose.Schema({
 	name: String,
+
+	created: {type: Date, default: Date.now},
+	modified: {type: Date, default: Date.now},
+
 	main: {
 		make: {type: String, default: ""},
 		model: {type: String, default: ""},
@@ -12,8 +18,13 @@ var rigSchema = new mongoose.Schema({
 		model: {type: String, default: ""},
 		size: {type: Number, default: 0}
 	},
-	created: {type: Date, default: Date.now},
-	modified: {type: Date, default: Date.now}
+
+	approvedUsers: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		}
+	]
 });
 
 module.exports = mongoose.model("Rig", rigSchema);
