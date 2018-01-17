149a5cbd5f7e1a71eb846aaa9c22b9bb4eefb634
Max Fuller <max.fuller01@btinternet.com>
2018-01-17 15:32:35 +0000

    Adde some additional info to user model

--- website/models/user.js
+++ website/models/user.js
@@ -2,14 +2,24 @@ var mongoose 				= require("mongoose"),
 	passportLocalMongoose 	= require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
+	// Passport required
 	username: String,
 	password: String,
 
+	// Additional info
 	email: String,
-
 	forename: String,
 	surname: String,
 
+	// User level
+	isAdmin: {type: Boolean, default: false},
+	isCommittee: {type: Boolean, default: false},
+
+	// Membership
+	hasMembership: {type: Boolean, default: false},
+
+	// Privileges
+	canEditRigs: {type: Boolean, default: false},
 
 	// approvedRigs: [
 	// 	{
