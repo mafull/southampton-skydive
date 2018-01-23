0b813e5ba6f7839afb61b5b624f26074aa9c9f2a
Max Fuller <max.fuller01@btinternet.com>
2018-01-23 18:12:11 +0000

    Updated user show formatting

--- website/routes/users.js
+++ website/routes/users.js
@@ -21,34 +21,20 @@ router.get("/", function(req, res) {
 
 // Show
 router.get("/:id", function(req, res) {
-	User.findById(req.params.id).populate("approvedRigs").exec(function(err, foundUser) {
+	User.findById(req.params.id).populate({
+		path: "approvedRigs",
+		options: {
+			// Sort biggest to smallest, docile to sporty
+			sort: {
+				"main.size": -1,
+				"name": 1
+			}
+		}
+	}).exec(function(err, foundUser) {
 		if(err) {
 			req.flash("error", err.message);
 			return res.redirect("/users");
 		}
-
-		//if(foundUser.surname === "Fuller") {
-			// Rig.findOne({}, function(err, foundRig) {
-			// 	if(err) {
-			// 		return console.log(err);
-			// 	}
-
-			// 	console.log("FOUND RIG: " + foundRig);
-
-			// 	if(!Array.isArray(foundRig.approvedUsers)) {
-			// 		foundRig.approvedUsers = [];
-			// 	}
-			// 	foundRig.approvedUsers.push(foundUser._id);
-			// 	foundRig.save(function(err, data) {
-			// 		if(err) {
-			// 			return console.log(err);
-			// 		}
-
-			// 		// DO SOMETHING
-			// 	});
-			// });
-		//}
-		
 
 		res.render("users/show", {user: foundUser});
 	});	
