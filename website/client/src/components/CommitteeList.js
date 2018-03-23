416c2d8429f5b3442af886db6d6fa3141253305a
Max Fuller <max.fuller01@btinternet.com>
2018-03-23 16:54:27 +0000

    Finish CommitteeList

--- website/client/src/components/CommitteeList.js
+++ website/client/src/components/CommitteeList.js
@@ -1,58 +1,58 @@
-import React, { Component } from "react"
-import CommitteeListElement from "./CommitteeListElement"
+import React, { Component } from "react";
+import CommitteeListElement from "./CommitteeListElement";
+import { Link } 				from "react-router-dom";
+import {
+	Header,
+	Icon,
+	Table,
+	Button
+}							from "semantic-ui-react";
 
-
-// TBODY
-/*
-	<% committeePositions.forEach(function(position) { %>
-							<tr>
-								<td>
-									<a href="/committee/<%= position._id %>"><%= position.name %></a>
-								</td>
-								<td>
-									<% if(position.user) { %>
-										<a href="/users/<%= position.user._id %>"><%= position.user.forename + " " + position.user.surname %></a>
-									<% } %>
-								</td>
-								<td>
-									<% if(position.user) { %>
-										<%= position.user.email %>
-									<% } %>
-								</td>
-							</tr>
-						<% }); %>
-*/
-
-
-// BELOW TABLE
-/*
-	<% if(currentUser && (currentUser.isAdmin || currentUser.isCommittee)) { %>
-					<a className="ui green button" href="/committee/new">Add comittee role</a>
-				<% } %>
-*/
 
 class CommitteeList extends Component {
+	constructor(props) {
+		super(props);
+
+		this.committeePositions = [
+			{
+				title: "Pres",
+				name: "Max F",
+				email: "one@two.com"
+			},
+			{
+				title: "VP",
+			},
+			{
+				title: "Dog",
+				name: "Ben",
+				email: "one@poo.com"
+			}
+		];
+	}
+
+
 	render() {
+		const committeeListElements = this.committeePositions ? this.committeePositions.map(p => <CommitteeListElement {...p} key={p.title} />) : null;
+		
 		return (
 			<div>
-				<div className="ui huge header"><i className="users icon"></i> Committee</div>
+				<Header size="huge"><Icon name="users" /> Committee</Header>
 				<p>View the current committee</p>
 
-				<table className="ui striped celled table">
-					<thead>
-						<tr>
-							<th>Position</th>
-							<th>Name</th>
-							<th>Email</th>
-						</tr>	
-					</thead>
-					<tbody>
-						<CommitteeListElement position="President" name="Max Fuller" email="mf5g14@soton.ac.uk" />
-						<CommitteeListElement />
-					</tbody>
-				</table>
+				<Table striped celled>
+					<Table.Header>
+						<Table.Row>
+							<Table.HeaderCell>Position</Table.HeaderCell>
+							<Table.HeaderCell>Name</Table.HeaderCell>
+							<Table.HeaderCell>Email</Table.HeaderCell>
+						</Table.Row>	
+					</Table.Header>
+					<Table.Body>
+						{committeeListElements}
+					</Table.Body>
+				</Table>
 
-				
+				<Button color="green" as={Link} to="/committee/new">Add committee position</Button>
 
 			</div>
 		);
