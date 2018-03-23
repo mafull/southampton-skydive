f31b7bd0d40bc04cf70661989b819ed6356214cf
Max Fuller <max.fuller01@btinternet.com>
2018-03-23 15:40:35 +0000

    Finish RigListElement for now

--- website/client/src/components/RigListElement.js
+++ website/client/src/components/RigListElement.js
@@ -1,38 +1,57 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
+import React, { Component } from "react";
+import PropTypes 			from "prop-types";
+import { Link } 			from "react-router-dom";
+import {
+	Grid,
+	Header,
+	Item,
+	Button
+}							from "semantic-ui-react";
 
 
 class RigListElement extends Component {
 	static propTypes = {
-		name: PropTypes.string.isRequired
+		name: PropTypes.string.isRequired,
+		equipment: PropTypes.shape({
+			main: PropTypes.shape({
+				model: PropTypes.string,
+				size: PropTypes.number
+			})
+		})
 	};
 
 
 	static defaultProps = {
-		name: "n/a"
+		name: "n/a",
+		equipment: {
+			main: {
+				model :"",
+				size: ""
+			}
+		}
 	};
 
 
 	render() {
-		const {name} = this.props;
+		const { id = 69, name, equipment: { main } } = this.props;
 
 		return (
-			<div className="four wide column">
-				<div className="item">
-					<div className="content">
-						<a className="ui huge header" href="/rigs/<%= rig._id %>">{name}</a>
-						<div className="meta">
-							<span>MODEL + SIZE</span>
-						</div>
-						<div className="description">
+			<Grid.Column width="4">
+				<Item>
+					<Item.Content>
+						<Header size="huge" as={Link} to={"/rigs/" + id}>{name}</Header>
+						<Item.Meta>
+							<span>{main.model} {main.size}</span>
+						</Item.Meta>
+						<Item.Description>
 
-						</div>
-						<div className="extra">
-							<a className="ui floated basic blue button" href="/rigs/<%= rig._id %>">Select</a>
-						</div>
-					</div>
-				</div>
-			</div>
+						</Item.Description>
+						<Item.Extra>
+							<Button as={Link} to={"/rigs/" + id} basic color="blue">Select</Button>
+						</Item.Extra>
+					</Item.Content>
+				</Item>
+			</Grid.Column>
 		);
 	}
 }
