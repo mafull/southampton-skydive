d584d3490794454814be9f193047dcafb43eb6ff
Max Fuller <max.fuller01@btinternet.com>
2018-03-24 01:15:11 +0000

    Remove unused state from App.js

--- website/client/src/App.js
+++ website/client/src/App.js
@@ -5,31 +5,6 @@ import CentralComponent 	from "./components/CentralComponent";
 import Footer 				from "./components/Footer";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			users: [
-				{
-					name: "Max Fuller",
-					email: "mf5g14@soton.ac.uk"
-				},
-				{
-					name: "Alex Potter",
-					email: "awmp1g14@soton.ac.uk"
-				}
-			],
-			rigs: [
-				{
-					name: "jav",
-
-				},
-				{
-					name: "wings",
-					
-				}
-			],
-		};
-	}
 	render() {
 		return (
 			<div>
