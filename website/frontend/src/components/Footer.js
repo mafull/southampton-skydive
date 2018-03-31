17321f69bcd77d7c5980f41bd594c47f6d89f4d3
Max Fuller <max.fuller01@btinternet.com>
2018-03-31 14:15:09 +0100

    Fix overal formatting

--- website/frontend/src/components/Footer.js
+++ website/frontend/src/components/Footer.js
@@ -4,7 +4,13 @@ import React, { Component } from "react";
 class Footer extends Component {
 	render() {
 		return (
-			<p style={{textAlign: "center"}}><em>{"\u00A9"} 2018 - Max Fuller, University of Southampton Skydive Club</em></p>
+			<p
+				style={{
+					textAlign: "center",
+					paddingBottom: "15px"
+				}}>
+				<em>{"\u00A9"} 2018 - Max Fuller, University of Southampton Skydive Club</em>
+			</p>
 		);
 	}
 };
